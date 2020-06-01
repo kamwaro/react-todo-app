e12837802cf9f327296da8ad4c2a776c5cc4829f
kamwaro <petersonkamwaro@gmail.com>
2020-06-01 13:51:10 +0300

    learning react

---
+++ src/Components/Todoitem.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+    };
+  };
+
+  render() {
+    const { id, title } = this.props.todo;
+
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type='checkbox'
+            onChange={this.props.markComplete.bind(this, id)}
+          />
+          {title}
+
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '6px 10px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right',
+};
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
+
+export default TodoItem;
